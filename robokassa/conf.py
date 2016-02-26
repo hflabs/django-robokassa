3bbbc473cd7bef16297b94133350eda667d76d29
alrusdi <alrusdi@gmail.com>
2016-02-26 14:58:35 +0500

    Removed not working test.robokassa.ru from default configuration

--- robokassa/conf.py
+++ robokassa/conf.py
@@ -17,14 +17,11 @@ STRICT_CHECK = getattr(settings, 'ROBOKASSA_STRICT_CHECK', True)
 TEST_MODE = getattr(settings, 'ROBOKASSA_TEST_MODE', False)
 
 # url, по которому будет идти отправка форм
-FORM_TARGET = u'https://merchant.roboxchange.com/Index.aspx'
-
-if TEST_MODE:
-    FORM_TARGET = getattr(
-        settings, 
-        'ROBOKASSA_TEST_FORM_TARGET', 
-        u'http://test.robokassa.ru/Index.aspx'
-    )
+FORM_TARGET = getattr(
+    settings,
+    'ROBOKASSA_TEST_FORM_TARGET',
+    u'https://merchant.roboxchange.com/Index.aspx'
+)
 
 # список пользовательских параметров ("shp" к ним приписывать не нужно)
 EXTRA_PARAMS = sorted(getattr(settings, 'ROBOKASSA_EXTRA_PARAMS', []))
